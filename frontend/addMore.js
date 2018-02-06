a6e3a79d3f4f50470cf2df89f94980b09fc7a06f
Karlos Muradyan <carlosmuradyan2@gmail.com>
2018-02-07 00:57:42 +0400

    Adding additional users is now available by clocking on "add more".
    Additionally some cosmetic changes are done in the code.

---
+++ frontend/addMore.js
@@ -0,0 +1,14 @@
+var i = 8;
+var colors = ["w3-text-blue", "w3-text-gray", "w3-text-green", "w3-text-black",
+                            "w3-text-orange", "w3-text-red", "w3-text-yellow"];
+
+function showOld(){
+        var appendFields1 = '<tr>';
+        appendFields1 +='<td><i class="fa fa-user ' + colors[(i-1)%7] + ' w3-large"></i></td>';
+        appendFields1 +='<td>User '+ i +'</td>';
+        appendFields1 +='<td><input type="number" class="w3-input w3-transparent" placeholder="Voted value"></td>';
+        appendFields1 +='<td><input type="number" class="w3-input w3-transparent" placeholder="Amount bided"></td>';
+        appendFields1 += '</tr>';
+        $("#tableo").append(appendFields1);
+        i++;
+}
