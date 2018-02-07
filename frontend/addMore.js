8af3877c672d376713b08786df2fd56adffe0b16
Karlos Muradyan <carlosmuradyan2@gmail.com>
2018-02-07 18:06:58 +0400

    The connection between frontend and backend is created. Now after clocking submit button, the values of input fields are passed to the server.

--- frontend/addMore.js
+++ frontend/addMore.js
@@ -6,8 +6,8 @@ function showOld(){
         var appendFields1 = '<tr>';
         appendFields1 +='<td><i class="fa fa-user ' + colors[(i-1)%7] + ' w3-large"></i></td>';
         appendFields1 +='<td>User '+ i +'</td>';
-        appendFields1 +='<td><input type="number" class="w3-input w3-transparent" placeholder="Voted value"></td>';
-        appendFields1 +='<td><input type="number" class="w3-input w3-transparent" placeholder="Amount bided"></td>';
+        appendFields1 +='<td><input type="number" name="user_data" class="w3-input w3-transparent" placeholder="Voted value"></td>';
+        appendFields1 +='<td><input type="number" name="user_bid" class="w3-input w3-transparent" placeholder="Amount bided"></td>';
         appendFields1 += '</tr>';
         $("#tableo").append(appendFields1);
         i++;
